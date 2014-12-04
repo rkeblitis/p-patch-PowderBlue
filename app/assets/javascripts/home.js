ad2475c7dad8a06975bc1105be5c1f80b1c3bebf
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-03 16:55:30 -0800

    create mock signup form that slides out with js/jquery

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,6 @@
+$(function() {
+  $("#signup").hide();
+  $(".yellow-text").click(function() {
+    $("#signup").slideToggle("slow");
+  });
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+    User.new(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password_digest)
+  end
+
 end

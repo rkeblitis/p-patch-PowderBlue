251634f32e526e1807510af28fcbf8c24541ad13
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-08 12:02:02 -0800

    add more sessions logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,13 @@ class SessionsController < ApplicationController
 
   def create
     @current_user = User.find_by(username: params[:username])
-    if @current_user.authenticate(params[:password])
-      redirect_to my_account_path, notice: "You're signed in, #{@current_user.username}. Woohoo!"
+    if @current_user != nil
+      if @current_user.authenticate(params[:password])
+        session[:user_id] = @current_user.id
+        redirect_to my_account_path(@current_user), notice: "You're signed in, #{@current_user.username}. Woohoo!"
+      end
+    else
+      redirect_to root_path, notice: "Couldn't sign you in! Are you sure you've signed up?"
     end
   end
 

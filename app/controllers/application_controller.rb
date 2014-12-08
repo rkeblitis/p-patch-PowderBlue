691f72ca7542141d952a2ddac424091ac61079a7
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-08 11:11:52 -0800

    fix application controller logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,27 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
-  helper_method :logged_in?
+  # helper_method :logged_in?
 
   def current_user
-    if User.find(params[:id])
-      @current_user = session[:user_id]
-    else
-      @current_user = nil
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
     end
+
+
+    # if User.find(params[:id])
+    #   @current_user = session[:user_id]
+    # else
+    #   @current_user
+    # end
   end
 
-  def logged_in?
-    if @current_user != nil
-      true
-    else
-      false
-    end
-  end
+  # def logged_in?
+  #   if @current_user != nil
+  #     true
+  #   else
+  #     false
+  #   end
+  # end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     @current_user = User.find_by(username: params[:username])
     if @current_user.authenticate(params[:password])
-      redirect_to root_path, notice: "You're signed in, #{@current_user.username}. Woohoo!"
+      redirect_to my_account_path, notice: "You're signed in, #{@current_user.username}. Woohoo!"
     end
   end
 
@@ -15,22 +15,23 @@ class SessionsController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     if Oauth.find_by(uid: auth_hash["uid"])
       @oauth = Oauth.find_by(uid: auth_hash["uid"])
-      @current_user = session[@oauth.user_id]
+      session[:user_id] = @oauth.user_id
       @user = User.find(@oauth.user_id)
-      redirect_to root_path, notice: "You're signed in, #{@user.username} Yay!"
+      redirect_to my_account_path(@user), notice: "You're signed in, #{@user.username} Yay!"
     else
       @user = User.create(username: auth_hash["info"]["nickname"], password_digest: "nil", email: "nil")
-      Oauth.create(user_id: @user.id, uid: auth_hash["uid"], provider: auth_hash["provider"], token: auth_hash["credentials"]["token"])
-      @current_user = session[@user.id]
-      redirect_to root_path, notice: "You're signed in, #{@user.username} Yay!"
+      @oauth = Oauth.create(user_id: @user.id, uid: auth_hash["uid"], provider: auth_hash["provider"], token: auth_hash["credentials"]["token"])
+      session[:user_id] = @oauth.user_id
+      redirect_to my_account_path(@user), notice: "You're signed in, #{@user.username} Yay!"
     end
   end
 
   def show
+
   end
-  
+
   def destroy
     session[:user_id] = nil
-    redirect_to root_path, notice: "Signed Out"
+    redirect_to root_path, notice: "Signed Out."
   end
 end

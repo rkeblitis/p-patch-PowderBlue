d7fc126d2ce38f2ca4ab2ed45965f1ecf2f4e00d
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-09 12:02:10 -0800

    place calendar on front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
   def index
     @current_user = User.new
+    @beginning_of_month = Date.civil(2014, 12, 1)
+    @end_of_month = Date.civil(2014, 12, -1)
   end
 
 end

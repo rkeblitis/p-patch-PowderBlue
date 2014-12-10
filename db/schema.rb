5553e16321311e44a6bb76e48a36c916f5c547d6
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-10 11:23:55 -0800

    add date column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141209175647) do
+ActiveRecord::Schema.define(version: 20141210192317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,11 +44,11 @@ ActiveRecord::Schema.define(version: 20141209175647) do
 
   create_table "events", force: true do |t|
     t.string   "name"
-    t.datetime "datetime"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.date     "date"
   end
 
   create_table "oauths", force: true do |t|

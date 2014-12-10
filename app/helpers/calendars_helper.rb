47b79d5c92bc685d0e7a8461b203fe50be53040c
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-09 16:57:00 -0800

    add notes to self about calendar

--- app/helpers/calendars_helper.rb
+++ app/helpers/calendars_helper.rb
@@ -1,8 +1,8 @@
 module CalendarsHelper
 
   def find_events_for_month(year, month)
-    # date = DateTime.parse("#{year}-#{month}")
-    # @events = Event.all.where("datetime ILIKE ?", "%date%")
+      # date = DateTime.parse("#{year}-#{month}")
+      # @events = Event.all.where("datetime ILIKE ?", "%date%")
   end
 
 end

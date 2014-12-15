6cc89cf0c44f69fa92d15d4147586dabb386b3cc
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-14 22:46:31 -0800

    refine mailer logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,7 +24,7 @@ class SessionsController < ApplicationController
       @user = User.find(@oauth.user_id)
       redirect_to my_account_path, notice: "You're signed in, #{@user.username}. Yay!"
     else
-      @user = User.create(username: auth_hash["info"]["nickname"], password_digest: "nil", email: "nil")
+      @user = User.create(username: auth_hash["info"]["nickname"], password_digest: "nil", email: nil)
       @oauth = Oauth.create(user_id: @user.id, uid: auth_hash["uid"], provider: auth_hash["provider"], token: auth_hash["credentials"]["token"])
       session[:user_id] = @oauth.user_id
       redirect_to my_account_path, notice: "You're signed in, #{@user.username}. Yay!"

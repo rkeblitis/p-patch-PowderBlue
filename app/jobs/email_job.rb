ce216e31df8c6e769b53372c68cef92095426a91
Rachelle Keblitis <rkeblitis@gmail.com>
2014-12-14 17:12:13 -0800

    add mailer

---
+++ app/jobs/email_job.rb
@@ -0,0 +1,6 @@
+class EmailJob
+  @queue = :email
+  def self.perform
+    NewsMailer.news_update(post_id).deliver
+  end
+end
--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -1,14 +1,16 @@
 class NewsMailer < ActionMailer::Base
   default from: "rkeblitis@gmail.com"
 
-  def mail
-    @news = Post.last
-    #User.all.each do |user|
-      mail(to: "rkeblitis@gmail.com",
+  def news_update(post_id)
+    @news = Post.find(post_id)
+    User.all.each do |user|
+      mail(to: user.email,
           subject: "Moar News for ",
           body: "#{@news.title}\n read more: \n http://localhost:3000/posts/#{@news.id}"
           )
-    #end
+    end
+
+    Resque.enqueue(EmailJob, @news)
   end
 
 

3b2917c6d238aba1844b182a142d6a87d056fe96
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-14 21:57:54 -0800

    delete development.rb config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,17 +33,17 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
 
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.mandrillapp.com',
-    port: 587,
-    domain: 'example.com',
-    user_name: ENV["MANDRILL_USERNAME"],
-    password: ENV["MANDRILL_PASSWORD"],
-    authentication: 'plain',
-    enable_starttls_auto: true
-    }
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address: 'smtp.mandrillapp.com',
+  #   port: 587,
+  #   domain: 'example.com',
+  #   user_name: ENV["MANDRILL_USERNAME"],
+  #   password: ENV["MANDRILL_PASSWORD"],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true
+  #   }
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true

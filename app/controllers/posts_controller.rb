f45586f8769f680f216a04f298ff089729fc5b8a
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-15 00:16:41 -0800

    change redirect

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,9 +4,9 @@ class PostsController < ApplicationController
     @post = Post.create(params.require(:post).permit(:title, :content, :user_id))
     if @post.save
       NewsMailer.news_update(@post.id).deliver
-      redirect_to admin_path
+      redirect_to my_account_path
     else
-      redirect_to admin_path
+      redirect_to my_account_path
     end
   end
 

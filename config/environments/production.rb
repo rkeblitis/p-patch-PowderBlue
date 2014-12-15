1f5f9e55a940fa723c68f7711fee91fc684f9d79
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-14 22:33:31 -0800

    mandrill smtp keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,18 +1,21 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.gem 'redis'
-  
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     address: 'smtp.mandrillapp.com',
     port: 587,
     domain: 'example.com',
     user_name: ENV["MANDRILL_USERNAME"],
-    password: ENV["MANDRILL_PASSWORD"],
+    password: ENV["MANDRILL_APIKEY"],
+    # password: ENV["MANDRILL_PASSWORD"],
     authentication: 'plain',
     enable_starttls_auto: true
   }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Code is not reloaded between requests.
   config.cache_classes = true

b6be0c761cb333eaccfbbd45baa6f8a0f2c7256e
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-14 21:43:16 -0800

    add resque redis stuff

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,2 @@
-# uri = URI.parse(ENV["REDISTOGO_URL"])
-# Redis = Redis.new(:url => ENV["REDISTOGO_URL"])
+uri = URI.parse(ENV["REDISTOGO_URL"])
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
--- lib/tasks/resque.rake
+++ lib/tasks/resque.rake
@@ -1,2 +1,3 @@
-require "resque/tasks"
+require 'resque/tasks'
+
 task "resque:setup" => :environment

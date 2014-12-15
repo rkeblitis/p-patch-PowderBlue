f98512910c08c03cbc597af3c375a47b48e07319
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-14 21:47:14 -0800

    adjust resque file

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,4 @@
-uri = URI.parse(ENV["REDISTOGO_URL"])
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+if ENV.include?('REDISTOGO_URL')
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end

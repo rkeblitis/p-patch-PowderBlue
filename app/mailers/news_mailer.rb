fa5b403ba824068ab9745f10890bb31d7f65ecb1
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-14 21:34:52 -0800

    add more styling

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -1,11 +1,6 @@
 class NewsMailer < ActionMailer::Base
   default to: Proc.new{User.pluck(:email)}
   default from: "sk@stephaniekwak.com"
-  # def join_us
-  #   @news = Post.first
-  #   mail(to: "bookis.smuin+club@gmail.com", subject: "Come join #{@news.title} for a dance party!")
-  # end
-
 
   def news_update(post_id)
     @news = Post.find(post_id)

584e5624f882c3c0194edfb16a684ca6a92592f1
Rachelle Keblitis <rkeblitis@gmail.com>
2014-12-14 19:17:32 -0800

    fixed something

--- app/mailers/news_mailer.rb
+++ app/mailers/news_mailer.rb
@@ -7,7 +7,7 @@ class NewsMailer < ActionMailer::Base
     #User.all.each do |user|
       mail(
           subject: "Moar News for ",
-          body: "#{@news.title}\n read more: \n http://localhost:3000/posts/#{@news.id}"
+          body: "#{@news.title}\n read more: \n http://can-you-dig-it.herokuapp.com/posts/#{@news.id}"
           )
 
     Resque.enqueue(EmailJob, @news)

7401a70de0eeab654c33d0e84a156ffc3d0ef032
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-14 23:49:11 -0800

    change production file again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,21 +2,18 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.gem 'redis'
 
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.mandrillapp.com',
-    port: '587',
-    domain: 'heroku.com',
-    user_name: ENV["MANDRILL_USERNAME"],
-    password: ENV["MANDRILL_APIKEY"],
-    # password: ENV["MANDRILL_PASSWORD"],
-    authentication: 'plain',
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mandrillapp.com',
+    port:                 587,
+    domain:               'heroku.com',
+    user_name:            ENV["MANDRILL_USERNAME"],
+    password:             ENV["MANDRILL_PASSWORD"], # your gmail password
+    authentication:       'plain',
     enable_starttls_auto: true
   }
-  ActionMailer::Base.delivery_method = :smtp
-
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 

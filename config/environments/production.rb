66ab79066d0181aab9ecd7210df9eed43454f4d6
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2014-12-14 21:53:36 -0800

    add config stuff to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
